07c3e57c57a512238598aa47aa83b7c1d916ed30
VitorRBrumatti <vitorrufino084@gmail.com>
2024-04-23 15:34:41 -0300

    alterations

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="detail">a</div>
+</template>
+
+<style scoped>
+ .detail {
+    width: 609px;
+    height: 599px;
+    top: 31px;
+    left: 31px;
+    gap: 0px;
+    opacity: 0px;
+    background: #F7F7F7;
+}
+</style>
